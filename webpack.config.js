54353e4b71a678ce3066924a3746109b4dc2fae8
Bram Borggreve <beeman@beeman.nl>
2023-06-21 17:13:40 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const webpack = require('webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-  config.resolve.fallback = {
-    process: false,
-  };
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      process: {
+        browser: true,
+      },
+    })
+  );
   return config;
 });
